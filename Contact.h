2b232e75b5a06581c5ff23ea7afcb171022619a4
Hafsa Maryam <hafsamaryam6789@gmail.com>
2025-12-09 15:30:35 +0500

    Add Contact class header for phone book project

--- Contact.h
+++ Contact.h
@@ -2,9 +2,26 @@
 #define CONTACT_H
 #include <string>
 using namespace std;
+class Contact{
+private:
+string name;
+string phone;
 
-struct Contact{
-    string name;
-    string phone;
-};
+public:
+//Constructors
+Contact(){
+}
+Contact(const string& name, const string& phone);
+//Getter
+string getName() const;
+string getPhone() const;
+//Setter
+void setName(const string& name);
+void setPhone(const string& phone);
+//for saving as string
+string toString() const;
+static Contact FromString(const string& line);
+
+
 #endif
+};

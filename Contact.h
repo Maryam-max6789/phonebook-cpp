de72b4f35dabce1964d96111dca6fe04b0b18829
Hafsa Maryam <hafsamaryam6789@gmail.com>
2025-12-09 13:08:32 +0500

    Initial phoneBook Project Structure

---
+++ Contact.h
@@ -0,0 +1,10 @@
+#ifndef CONTACT_H
+#define CONTACT_H
+#include <string>
+using namespace std;
+
+struct Contact{
+    string name;
+    string phone;
+};
+#endif
---
+++ phoneBook.h
@@ -0,0 +1,10 @@
+#ifndef PHONEBOOK_H
+#define PHONEBOOK_H
+
+
+void addContact();
+void showContact();
+void searchContact();
+void deleteContact();
+
+#endif
